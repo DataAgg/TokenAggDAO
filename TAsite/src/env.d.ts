361a79198841764e88911a451f6db4ae27b82a74
watano <watano@qq.com>
2022-08-25 22:07:14 +0800

    fix bugs

---
+++ TAsite/src/env.d.ts
@@ -0,0 +1,22 @@
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-pages/client" />
+
+declare interface Window {
+  // extend the window
+}
+
+interface ImportMetaEnv {
+  VITE_GTM_ID: string
+}
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+declare module '*.md' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
--- TAsite/src/router/index.ts
+++ TAsite/src/router/index.ts
@@ -1,6 +1,7 @@
 import type { App } from "vue";
 import { createRouter, createWebHistory } from "vue-router";
 import routes from "~pages";
+// import routes from 'virtual:generated-pages';
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
